862831219278feb44b0b17aa66c1c444c16a5add
Brandon Lampe <brandon.lampe@gmail.com>
2016-06-09 11:13:13 -0600

    Added ability to plot results from CS Model for comparison
    - when the results from CS_SAND98_2680.py are saved to a .csv file, these
      results may by setting the variable PLOT_CSMOD = 1 and defining the correct
      file path (PATH_CSMOD) of the .csv file
    
    - fitting results for the function which models the fractional density (FDEN)
      may now be saved as a .csv file.  These results will automatically be saved
      when SAVECSV == 1.

--- PythonCode/DataAnalysis.py
+++ PythonCode/DataAnalysis.py
@@ -6,7 +6,7 @@ from matplotlib.ticker import FormatStrFormatter
 from matplotlib.ticker import FuncFormatter
 from scipy import interpolate
 # from scipy import optimize
-# from scipy import linalg
+from scipy import linalg
 # import lmfit
 
 # relative path to module
@@ -17,9 +17,12 @@ REPO_DIR = os.path.dirname(sys.path[0])  # PATH TO REPOSITORY DIRECTORY
 ##################################
 # CHOOSE ANALYSIS OPTIONS
 ##################################
-PLOT = 1
-SAVEFIG = 1
-SAVECSV = 0
+PLOT = 0
+SAVEFIG = 0
+SAVECSV = 1
+PLOT_RESID = 1
+PLOT_CSMOD = 1  # PLOT RESULTS FROM CS MODEL, MUST DEFINE FILE TO LOAD DATA
+PATH_CSMOD = '/Users/Lampe/GrantNo456417/Modeling/constit/UNM_WP_HY_175_09_OUT.csv'
 
 DUR_START = 0.006  # START PLOTTING
 DUR_END = 0.95  # END PLOTTING
@@ -35,7 +38,7 @@ INTERP_INC = 10  # SECONDS, SIZE OF INTERPOLATION INCREMENT
 FOLDER_DIR = 'UNM_WP_HY_175_09'
 
 # load tests data - .csv file that has been exported directly from .xlsx
-TESTNAME = FOLDER_DIR[-6:]
+TEST_NAME = FOLDER_DIR[-6:]
 
 # DEFINE COLUMNS FOR:
 # 'UNM_WP_HY_175_09'
@@ -48,7 +51,7 @@ COL_FDEN = 31
 # print(daf.col_lbl(31))
 RHOIS = 2160.0  # ASSUMED IN-SITU DENSITY (KG/M3), FOR STRAIN MEASURE
 USECOLS = (np.arange(0, 32, 1))
-IMPORT_FNAME = REPO_DIR + '/' + FOLDER_DIR + '/' + TESTNAME + '.csv'
+IMPORT_FNAME = REPO_DIR + '/' + FOLDER_DIR + '/' + TEST_NAME + '.csv'
 # print("Imported Test: " + TESTNAME)
 print("File Path: " + IMPORT_FNAME)
 
@@ -108,17 +111,9 @@ print("Avg. Confining Pressure (psi): " + str(PCON_AVG_PSI))
 print("Avg. Pore Pressure (psi): " + str(PPOR_AVG_PSI))
 print('Avg. Temperature (C): ' + str(TEMP_AVG_C))
 
-# calculated volume strain rate
+# calculated volume strain rate by fitting model to fractional density
 FDEN0 = FDEN_INTERP[0]  # INITIAL FRACTIONAL DENSITY
-# VSTRN = -np.log(FDEN0 / FDEN_INTERP)  # "TRUE" VOL. STRN, + IN COMPRESSION
-# # np.gradient() uses a central difference for interior nodes
-# DELTA_VSTRN = np.gradient(VSTRN)  # incremental change in Vol. strn
-# DELTA_SEC = np.gradient(DUR_SEC)  # incremental time step
-# DELTA_SEC_INTERP = np.gradient(DUR_SEC_INTERP)  # incremental time step
-# VSTRN_RATE = DELTA_VSTRN / DELTA_SEC_INTERP  # SEC^-1
-
-# FIT PARAMETER VALUES TO SPECIFIED TEST DATA
-MODEL = daf.fit_fden(FDEN_INTERP, DUR_DAY_INTERP)
+MODEL = daf.fit_fden(FDEN_INTERP, DUR_DAY_INTERP)  # perform the FIT
 SLOPE, C, B, G, KEXP, AEXP, AZERO, AINF = MODEL.best_values.values()
 PARM_FIT = MODEL.best_values.values()
 # print(PARM_FIT)
@@ -129,9 +124,16 @@ PARM_FIT = MODEL.best_values.values()
 # plt.figure(0)
 # MODEL.plot_fit()
 # plt.show
+FIT_REPORT = MODEL.fit_report()
+# FIT_REPORT_CI = MODEL.ci_report()
 
-# plt.figure(0)
-# MODEL.plot_residuals()
+FIT_CI = MODEL.ci_out
+FIT_RESID = MODEL.residual
+FIT_RESID_NORM = linalg.norm(FIT_RESID)
+print("Error Norm of Fit: " + str(FIT_RESID_NORM))
+
+plt.figure(0)
+MODEL.plot_residuals()
 # plt.show()
 
 FDEN_FIT_INTERP = MODEL.eval(x=DUR_DAY_INTERP,
@@ -168,6 +170,29 @@ PSOL_INTERP = (PCON_INTERP - PPOR_INTERP * (1 - FDEN_INTERP)) / FDEN_INTERP
 ##################################
 # PLOTTING/EXPORTING BELOW
 ##################################
+# if PLOT_RESID == 1:
+
+# LOAD DATA FROM CS MODEL SIMULATION TO PLOT AGAINST EXPERIMENTAL RESULTS
+if PLOT_CSMOD == 1:
+    COL_CSTIME_SEC = 1
+    COL_CSFDEN = 9
+    COL_CSMSTRS = 15
+    COL_CSDSTRS = 16
+    COL_CSTEMP = 23
+    COL_CSVSTRN_RATE = 26
+    COL_CSMOD_IMPORT = (COL_CSTIME_SEC, COL_CSFDEN, COL_CSTEMP,
+                        COL_CSVSTRN_RATE, COL_CSMSTRS, COL_CSDSTRS)
+    CSMOD_ARR = np.genfromtxt(fname=PATH_CSMOD, delimiter=',', skip_header=2,
+                              usecols=COL_CSMOD_IMPORT, dtype=float)
+    print("CS Model data loaded")
+    CS_DUR_DAY = DUR_START + CSMOD_ARR[:,0] / (3600 * 24)  # PLOTTING TIME
+    CS_FDEN = CSMOD_ARR[:,1]
+    CS_TEMP = CSMOD_ARR[:,2]
+    CS_VSTRN_RATE = CSMOD_ARR[:,3] * (-1)
+    CS_PCON = CSMOD_ARR[:,3]
+    CS_PDEV = CSMOD_ARR[:,4]
+
+##################################
 FS = 14  # FONT SIZE FOR PLOTTING
 NUM_SUBPLOT = 3
 FIG1, AXARR = plt.subplots(NUM_SUBPLOT, figsize=(13, 10), sharex=True)
@@ -201,15 +226,19 @@ AXARR[0].set_title("Test: " + FOLDER_DIR,
 # APPROXIMATE VOLUME STRAIN AND FRACTIONAL DENSITY
 # #################################
 
-LBL_FDEN = ["Measured", "Fit"]
+LBL_FDEN = ["Measured", "Fit", "CS Model"]
 # AXARR[0].plot(DUR_DAY_INTERP, FDEN_INTERP, linestyle='-',
 #               linewidth=1, marker='.', markersize=4, color='g',
 #               alpha=1)
 AXARR[0].plot(DUR_DAY, FDEN, linestyle='-', linewidth=1,
-              marker='.', markersize=12, color='r', alpha=1)
+              marker='.', markersize=14, color='r', alpha=1)
 AXARR[0].plot(DUR_DAY_INTERP, FDEN_FIT_INTERP, linestyle='-',
-              linewidth=3, marker=None, markersize=1, color='b',
+              linewidth=4, marker=None, markersize=1, color='b',
               alpha=1)
+if PLOT_CSMOD == 1:
+    AXARR[0].plot(CS_DUR_DAY, CS_FDEN, linestyle='-',
+                  linewidth=2, marker=None, markersize=1, color='g',
+                  alpha=1)
 AXARR[0].grid(True)
 AXARR[0].set_ylabel("Fractional Density", fontsize=FS)
 AXARR[0].set_ylim(ymin=FDEN0)
@@ -228,17 +257,22 @@ AXARR[0].legend(LBL_FDEN, frameon=1, framealpha=1, loc=4, fontsize=FS)
 AXARR[0].tick_params(labelsize=FS)
 AXARR[0].tick_params(labelsize=FS, pad=10)
 #################################
-lbl_strnrate = ["Interpolated", "Measured", "Fit"]
+lbl_strnrate = ["Interpolated", "Measured", "Fit", "CS Model"]
 lbl_interval = ["Sampling"]
 AXARR[1].semilogy(DUR_DAY_INTERP, VSTRN_RATE_INTERP, linestyle='-',
                   linewidth=1, marker='.', markersize=12,
-                  color='g', alpha=1)
+                  color='y', alpha=1)
 AXARR[1].semilogy(DUR_DAY, VSTRN_RATE, linestyle='-',
                   linewidth=1, marker='.', markersize=12,
                   color='r', alpha=1)
 AXARR[1].semilogy(DUR_DAY_INTERP, VSTRN_RATE_FIT_INTERP, linestyle='-',
-                  linewidth=3, marker='None', markersize=12,
+                  linewidth=4, marker='None', markersize=12,
                   color='b', alpha=1)
+if PLOT_CSMOD == 1:
+    AXARR[1].semilogy(CS_DUR_DAY, CS_VSTRN_RATE, linestyle='-',
+                      linewidth=2, marker='None', markersize=12,
+                      color='G', alpha=1)
+
 AXARR[1].grid(True)
 AXARR[1].set_ylabel(r'Strain Rate (sec$^{-1}$)', fontsize=FS)
 
@@ -248,7 +282,7 @@ AX2B.semilogy(DUR_DAY, DELTA_SEC, linestyle='-',
               color='k', alpha=1)
 AX2B.set_ylabel("Seconds Per Sample", fontsize=FS)
 AX2B.tick_params(labelsize=FS)
-AXARR[1].legend(lbl_strnrate, frameon=1, framealpha=0.75, loc=3, fontsize=FS)
+AXARR[1].legend(lbl_strnrate, frameon=1, framealpha=0.75, loc=1, fontsize=FS)
 AX2B.legend(lbl_interval, frameon=1, framealpha=0.85, loc=4, fontsize=FS)
 
 AXARR[1].tick_params(labelsize=FS)
@@ -288,7 +322,7 @@ AXARR[LOWEST_CHART].tick_params(labelsize=FS, pad=10)
 # AXARR[LOWEST_CHART].set_xlim(DUR_START, DUR_END)
 
 # FIG1.tight_layout()
-FIG1_NAME = ("PLOTS_" + TESTNAME + "_" + str(DUR_START) + "-" +
+FIG1_NAME = ("PLOTS_" + TEST_NAME + "_" + str(DUR_START) + "-" +
              str(DUR_END) + '.pdf')
 PATH = REPO_DIR + '/' + FOLDER_DIR + '/'
 #################################
@@ -300,11 +334,18 @@ if PLOT != 0:
 if SAVECSV != 0:
     # SAVE RESULTS TO .CSV FILE
     OUT_FILENAME = PATH + TEST_NAME + '_OUT.csv'
+    OUT_FILENAME_REPORT = PATH + TEST_NAME + '_FITREPORT.csv'
+
     COMMENTS = """RESULTS FROM, XX, RUN BY, XX, ON, XX
                """
     HEADER = ('INC,TIME_SEC,ASTRNRATE,LSTRNRATE,ZETARATE,ASTRN,LSTRN,ZETA,\
                 VSTRN,FDEN,RHO,MDCREEP,SPCREEP,SSCREEP,FTRN,MSTRS,\
                 DSTRS,SEQ,SEQF,F2A,F2L,ASTRS,LSTRS,TEMPC,DSZ,WATER')
-    np.savetxt(OUT_FILENAME, OUT, fmt='%.8e', delimiter=',', newline='\n',
-               header=HEADER, comments=COMMENTS)
-    print("File Saved As: " + OUT_FILENAME)
+    # np.savetxt(OUT_FILENAME, OUT, fmt='%.8e', delimiter=',', newline='\n',
+    #            header=HEADER, comments=COMMENTS)
+    # print("Saved Data As: " + OUT_FILENAME)
+
+    OUT = open(OUT_FILENAME_REPORT, 'w')
+    OUT.write(FIT_REPORT)
+    # np.savetxt(OUT_FILENAME_REPORT, FIT_REPORT)
+    print("Saved Fit Report As: " + OUT_FILENAME_REPORT)
