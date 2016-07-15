82294b6c22e2244e36ae7c26ec96d9ff5fccf452
Brandon Lampe <brandon.lampe@gmail.com>
2016-07-15 09:15:02 -0600

    Completed formatting 175_09 and added MakeSummary.py
    MakeSummary.py:
    This routine pulls data from each of the stages that comprise 175_09 (creep
    only) and puts specific data into an array.  This "summary" array is then used
    to (1) create a table which is saved in a format suitable for LaTex and (2)
    creates a plot of specific summary data.

--- PythonCode/MakeSummary.py
+++ PythonCode/MakeSummary.py
@@ -1,9 +1,10 @@
 import sys
 import numpy as np
-import csv
-import itertools
 import os
 import pickle
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import Table
 
 # relative path to module
 sys.path.append(sys.path[0])
@@ -13,6 +14,8 @@ REPO_DIR = os.path.dirname(sys.path[0])  # PATH TO REPOSITORY DIRECTORY
 
 FOLDER_DIR = 'UNM_WP_HY_175_09'
 BUILD_SUMMARY = 0  # 0=> SUMMARY IS READ FROM FILE, 1=> SUMMARY IS BUILT
+WRITE_TABLE = 1
+SHOW_PLOT = 0
 # STAGE_DIR = '/' + STAGE_ID[1:]
 
 # load tests data - .csv file that has been exported directly from .xlsx
@@ -21,7 +24,7 @@ DIR_NAME = REPO_DIR + '/' + FOLDER_DIR
 SUMMARY_PATH = os.path.join(DIR_NAME, 'SUMMARY.bin')
 SUMMARY_CSV_PATH = os.path.join(DIR_NAME, 'SUMMARY.csv')
 
-if  BUILD_SUMMARY == 1:
+if BUILD_SUMMARY == 1:
     SUMMARY = np.zeros((22, 11))
     INC = 0
     # DISECT CREEP FIT REPORTS
@@ -31,11 +34,8 @@ if  BUILD_SUMMARY == 1:
             for root, fit_name, files in os.walk(stage_dir, topdown=True):
                 if fit_name != []:
                     if fit_name[0] == 'CREEP_FIT':
-                        # print(str(INC + 1))
-                        # print(fit_name[0])
                         fit_dir = os.path.join(root, 'CREEP_FIT')
                         for filename in os.listdir(fit_dir):
-                            # if filename[-13:] == 'FITREPORT.csv':
                             if filename[-7:] == 'OUT.csv':
                                 file_dir = os.path.join(fit_dir, filename)
                                 data = np.loadtxt(file_dir, delimiter=',',
@@ -46,8 +46,8 @@ if  BUILD_SUMMARY == 1:
                                 SUMMARY[INC, 3] = data[-1, 3]  # DURATION [DAY]
                                 SUMMARY[INC, 4] = data[0, 5]  # FITFDEN,START
                                 SUMMARY[INC, 5] = data[-1, 5]  # FITFDEN,END
-                                SUMMARY[INC, 6] = data[0, 7]  # STRNRATE,START[1/SEC]
-                                SUMMARY[INC, 7] = data[-1, 7]  # STRNRATE,END [1/SEC]
+                                SUMMARY[INC, 6] = data[0, 7]  # STRNRATE,START
+                                SUMMARY[INC, 7] = data[-1, 7]  # STRNRATE,END
                                 SUMMARY[INC, 8] = np.mean(data[:, 8])  # pcon
                                 SUMMARY[INC, 9] = np.mean(data[:, 9])  # ppore
                                 SUMMARY[INC, 10] = np.mean(data[:, 12])  # temp
@@ -61,81 +61,120 @@ else:
         SUMMARY = pickle.loads(handle.read())
 
 
-# print(SUMMARY)
+################################################
+# write latex table
+################################################
 
-import matplotlib.pyplot as plt
-from matplotlib import cm
+if WRITE_TABLE == 1:
+    TABLE_OUT = open('mytable.tex', 'w')
+    t = Table.Table(
+        numcols=11,
+        justs='lrccccccccc',
+        caption='Summary of results from',
+        label="tab:results")
+    t.add_header_row(['Stage', 'Time Start', 'Time End', 'Duration',
+                      'Initial $\\rho_f$', 'Final $\\rho_f$',
+                      'Initial $\\dot{e}$', 'Final $\\dot{e}$',
+                      '$P_{con}$', '$P_{por}$', 'Temperature'])
+    COL1 = SUMMARY[:, 0]
+    COL2 = SUMMARY[:, 1]
+    COL3 = SUMMARY[:, 2]
+    COL4 = SUMMARY[:, 3]
+    COL5 = SUMMARY[:, 4]
+    COL6 = SUMMARY[:, 5]
+    COL7 = SUMMARY[:, 6]
+    COL8 = SUMMARY[:, 7]
+    COL9 = SUMMARY[:, 8]
+    COL10 = SUMMARY[:, 9]
+    COL11 = SUMMARY[:, 10]
 
-LBND = 0.25  # lower bound on color map
+    t.add_data([COL1, COL2, COL3, COL4, COL5, COL6, COL7, COL8, COL9,
+                COL10, COL11], sigfigs=3)
+    t.print_table(TABLE_OUT)
+    TABLE_OUT.close()
+################################################
+# PLOTTING
+################################################
+LBND = 0.0  # lower bound on color map
 UBND = 1.0  # upper bound on color map
 
 LINE_NUM = SUMMARY.shape[0]
 COLORMAP_SUB = np.linspace(LBND, UBND, LINE_NUM)
 
-COLORMAP = [cm.cool(x) for x in COLORMAP_SUB]
+COLORMAP = [cm.Set1(x) for x in COLORMAP_SUB]
 
 FS = 16
-FIG1 = plt.figure()
-AX = FIG1.add_subplot(111)
+FIG1 = plt.figure(figsize=(13, 10))
+AX1 = FIG1.add_subplot(111)
+# subplot(rows, columns, plot number)
+AX1A = AX1.twinx()
 
-LINES = []
+STRNRATE_LINES = []
+PCON_LINES = []
 for idx, color in enumerate(COLORMAP):
-    XVAL = SUMMARY[idx, 4:6]
-    YVAL = SUMMARY[idx, 6:8]
-    LBL = "Stage: " + str(int(SUMMARY[idx, 0]))
-    current_line, = AX.semilogy(XVAL, YVAL, color=color, label=LBL,
-                                linestyle='-',
-                                linewidth=2, marker='o', markersize=3,
-                                alpha=1)
-    LINES.append(current_line)
+    FDEN = SUMMARY[idx, 4:6]
+    PCON = np.array((SUMMARY[idx, 8], SUMMARY[idx, 8]))
+    PPOR = np.array((SUMMARY[idx, 9], SUMMARY[idx, 9]))
+    PTER = PCON - PPOR
+    STRNRATE = SUMMARY[idx, 6:8]
+    LBL_STRNRATE = "Stage: " + str(int(SUMMARY[idx, 0]))
+    if idx == 0:
+        LBL_PCON = 'Confining Pressure'
+        LBL_PPOR = 'Pore Pressure'
+        LBL_PTER = 'Terzaghi Pressure'
+    else:
+        LBL_PCON = '_nolegend_'
+        LBL_PPOR = '_nolegend_'
+        LBL_PTER = '_nolegend_'
+    current_strnrate, = AX1.semilogy(FDEN, STRNRATE, color=color,
+                                     label=LBL_STRNRATE,
+                                     linestyle='-',
+                                     linewidth=3, marker='None', markersize=4,
+                                     markerfacecolor='k', fillstyle='bottom',
+                                     alpha=1)
+    current_pcon = AX1A.plot(FDEN, PCON, color='green', label=LBL_PCON,
+                             linestyle='-',
+                             linewidth=2, marker='o', markersize=6,
+                             markerfacecolor='green', fillstyle='none',
+                             alpha=1)
+    current_pcon = AX1A.plot(FDEN, PPOR, color='blue', label=LBL_PPOR,
+                             linestyle='-',
+                             linewidth=2, marker='o', markersize=6,
+                             markerfacecolor='blue', fillstyle='none',
+                             alpha=1)
+    current_pcon = AX1A.plot(FDEN, PTER, color='red', label=LBL_PTER,
+                             linestyle='-',
+                             linewidth=2, marker='o', markersize=6,
+                             markerfacecolor='red', fillstyle='none',
+                             alpha=.5)
+    STRNRATE_LINES.append(current_strnrate)
+    PCON_LINES.append(current_pcon)
 
-handles, labels = AX.get_legend_handles_labels()
-AX.legend(handles, labels, loc='lower left', ncol=2,
-          fontsize=10)
+AX1.set_title("Test: " + FOLDER_DIR, fontsize=18)
+AX1A.set_ylim(ymin=0)
+AX1.set_xlim(xmin=0.94, xmax=0.955)
+AX1.set_ylabel(r'Strain Rate $\left(\frac{1}{sec}\right)$', fontsize=FS)
+AX1.set_xlabel("Fractional Density", fontsize=FS)
+AX1A.set_ylabel("Pressure (MPa)", fontsize=FS)
+AX1.grid()
+AX1.tick_params(labelsize=FS, pad=10)
+AX1A.tick_params(labelsize=FS, pad=10)
 
-AX.set_xlim(xmin=0.8, xmax=.96)
-AX.set_ylabel(r'Strain Rate ($\frac{1}{sec}$)', fontsize=FS)
-AX.set_xlabel("Fractional Density", fontsize=FS)
-AX.grid()
-plt.show()
+HANDLES_1, LABELS_1 = AX1.get_legend_handles_labels()
+#  bbox_to_anchor(xmin, ymin, xmax, ymax) ro bounding box of legend
+LGD1 = AX1.legend(HANDLES_1, LABELS_1, bbox_to_anchor=(1.1, 0.4, 1.03, 1.0),
+                  loc='lower left', borderaxespad=1, ncol=1,
+                  fontsize=10)
+HANDLES_1A, LABELS_1A = AX1A.get_legend_handles_labels()
+LGD1A = AX1A.legend(HANDLES_1A, LABELS_1A,
+                    bbox_to_anchor=(1.1, 0.0, 1.03, 0.4),
+                    loc='upper left', borderaxespad=1, ncol=1,
+                    fontsize=10)
 
-# ##################################
-# FS = 14  # FONT SIZE FOR PLOTTING
-# ##################################
-# # # # print color list
-# # for name, hex in matplotlib.colors.cnames.iteritems():
-# #     print(name, hex)
 
-# ######################################################################
-# # PLOTTING DURING LOAD UP ONLY BELOW
-# ######################################################################
-# FIG1 = plt.figure(figsize=(13, 10))
+FIG1_NAME = 'TestSummary_94.pdf'
+FIG1_PATH = os.path.join(DIR_NAME, FIG1_NAME)
+FIG1.savefig(FIG1_PATH, bbox_extra_artists=(LGD1A,), bbox_inches='tight')
 
-# AX1 = FIG1.add_subplot(311)
-# AX1.set_title("Test: " + FOLDER_DIR, fontsize=18)
-
-# LBL_FDEN = ["Fractional Density: Loading"]
-# LBL_STRN = ["Volumetric Strain"]
-# LBL_PRES = ["Pressure: Confining"]
-# AX1.plot(LOAD_TIME_SEC, LOAD_FDEN, linestyle='-', linewidth=2,
-#          marker='s', markersize=4, color='cyan', alpha=1)
-# AX1.grid(True)
-
-# AX1A = AX1.twinx()
-# AX1A.plot(LOAD_TIME_SEC, LOAD_PCON, linestyle='-',
-#           linewidth=1, marker='.', markersize=8, color='r',
-#           alpha=1)
-# # AX1A.plot(LOAD_TIME_SEC, LOAD_PSOL, linestyle='-',
-# #           linewidth=1, marker='s', markersize=4, color='m',
-# #           alpha=1)
-# AX1A.tick_params(labelsize=FS)
-# AX1.tick_params(labelsize=FS, pad=10)
-
-# AX1.legend(LBL_FDEN, frameon=1, framealpha=1, loc=2, fontsize=FS)
-# AX1A.legend(LBL_PRES, frameon=1, framealpha=1, loc=4, fontsize=FS)
-# AX1.set_ylabel("Fractional Density", fontsize=FS)
-# AX1A.set_ylabel("Pressure (MPa)", fontsize=FS)
-# AX1.set_xlabel("Loading Time (sec)",
-#                fontsize=FS, labelpad=0)
-
-# #################################
+if SHOW_PLOT == 1:
+    plt.show()
