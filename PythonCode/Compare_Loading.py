f2489def85a1289615602eeeabdc83b1d0aa7624
Brandon Lampe <brandon.lampe@gmail.com>
2016-09-05 22:51:23 -0600

    completed graphical summary of loading up to 175_12

--- PythonCode/Compare_Loading.py
+++ PythonCode/Compare_Loading.py
@@ -13,12 +13,15 @@ import DataAnalysisFunc as daf
 REPO_DIR = os.path.dirname(sys.path[0])  # PATH TO REPOSITORY DIRECTORY
 
 SAVE_DIR = 'WP_HY_175'
-FOLDER_DIR_LIST = ['UNM_WP_HY_175_04',
-                   'UNM_WP_HY_175_09/STAGE01']
+FOLDER_DIR_LIST = ['UNM_WP_HY_175_09/STAGE01',
+                   'UNM_WP_HY_175_10/STAGE01',
+                   'UNM_WP_HY_175_11/STAGE01',
+                   'UNM_WP_HY_175_12/STAGE01']
+# 'UNM_WP_HY_175_04',
 
 # BUILD_SUMMARY = 1  # 0=> SUMMARY IS READ FROM FILE, 1=> SUMMARY IS BUILT
 SHOW_PLOT = 1
-SAVE_PLOT = 0
+SAVE_PLOT = 1
 
 OUT_DIR = os.path.join(REPO_DIR, 'SUMMARY', SAVE_DIR)
 # CREEP_PARM_LIST = []  # LIST OF dictS of - CREEP parameters
@@ -43,7 +46,20 @@ for folder_dir in FOLDER_DIR_LIST:
             fit_dir = os.path.join(root, fit_name[FIT_IDX])
             # print(fit_dir)
             for filename in os.listdir(fit_dir):
-                if filename[-7:] == 'OUT.csv':
+                print(test_name)
+                if test_name == '175_04':  # choose alternate file
+                    if filename[-11:] == 'OUT_MOD.csv':
+                        file_dir = os.path.join(fit_dir, filename)
+                        print(file_dir)
+                        arr = np.loadtxt(fname=file_dir, dtype=float,
+                                         delimiter=',', skiprows=3)
+                        # print(arr[:5,:])
+                        arr_name = test_name[:6]
+                        TEST_LIST.append(arr_name)
+                        DATA_DICT[arr_name] = arr
+                        size = len(DATA_DICT[arr_name])
+                        print(test_name)
+                elif filename[-7:] == 'OUT.csv':
                     file_dir = os.path.join(fit_dir, filename)
                     print(file_dir)
                     arr = np.loadtxt(fname=file_dir, dtype=float,
@@ -74,9 +90,19 @@ COLORMAP_SUB = np.linspace(LBND, UBND, LINE_NUM)
 COLORMAP = [cm.Set1(x) for x in COLORMAP_SUB]
 
 FIG1 = plt.figure(figsize=(13, 10))
-AX1 = FIG1.add_subplot(311)
-AX2 = FIG1.add_subplot(312)
-AX3 = FIG1.add_subplot(313)
+
+AX1 = FIG1.add_subplot(421)
+AX2 = FIG1.add_subplot(425)
+AX3 = FIG1.add_subplot(427, sharex=AX2)
+
+AX4 = FIG1.add_subplot(422)
+AX4A = AX4.twinx()
+AX5 = FIG1.add_subplot(424, sharex=AX4)
+AX5A = AX5.twinx()
+AX6 = FIG1.add_subplot(426, sharex=AX4)
+AX6A = AX6.twinx()
+AX7 = FIG1.add_subplot(428, sharex=AX4)
+AX7A = AX7.twinx()
 
 # CREATE LABELS FOR PLOTTING
 FOLDER_DIR_LIST_PLT = []
@@ -86,15 +112,20 @@ for i in FOLDER_DIR_LIST:
 
 STRNRATE_LINES = []
 STRN_LINES = []
-FDEN_LINES = []
+FDEN_TIME_LINES = []
+PPOR_TIME_LINES = []
+PCON_TIME_LINES = []
+PRES_TIME_LINES = []
+
 PCON_LINES = []
 PPOR_LINES = []
 PDIF_LINES = []
 TEMP_LINES = []
+
+plot_single = 0
 for idx, color in enumerate(COLORMAP):
-    print(COL_PCON)
-    print(idx)
     FDEN0 = DATA_DICT[TEST_LIST[idx]][0, COL_FDEN]
+    print("Initial Fractional Density: " + str(FDEN0))
     FDEN = DATA_DICT[TEST_LIST[idx]][:, COL_FDEN]
     PCON = DATA_DICT[TEST_LIST[idx]][:, COL_PCON]
     PPOR = DATA_DICT[TEST_LIST[idx]][:, COL_PPOR]
@@ -106,41 +137,105 @@ for idx, color in enumerate(COLORMAP):
     STRN_DELTA = np.gradient(STRN)
     TIME_DELTA = np.gradient(TIME)
     STRN_RATE = STRN_DELTA / TIME_DELTA
+    PRES_ARR = np.array((PCON, PPOR))
+    # print(PRES_ARR.shape)
 
     LBL_FDEN = "Test: " + TEST_LIST[idx]
     LBL_STRN = '_nolegend_'
     LBL_PDIF = '_nolegend_'
     LBL_PCON = '_nolegend_'
-#     if idx == 0:
-#         LBL_PCON = 'Confining Pressure'
-#         LBL_PPOR = 'Pore Pressure'
-#         LBL_PDIF = 'Pressure Difference'
-#     else:
-#         LBL_PCON = '_nolegend_'
-#         LBL_PPOR = '_nolegend_'
-#         LBL_PDIF = '_nolegend_'
+    LBL_PRES = [r'$P_c$', r'$P_p$']
 
-    current_fden = AX1.plot(TIME, FDEN, color=color, label=LBL_FDEN,
-                            linestyle='-',
-                            linewidth=3, marker='None', markersize=6,
-                            markerfacecolor=color, fillstyle='none',
-                            alpha=1)
-    current_pcon = AX2.plot(PCON, STRN, color=color,
+    current_fden_time = AX1.plot(TIME, FDEN, color=color, label=LBL_FDEN,
+                                 linestyle='-',
+                                 linewidth=3, marker='None', markersize=6,
+                                 markerfacecolor=color, fillstyle='none',
+                                 alpha=1)
+    current_pcon = AX2.plot(PDIF, STRN, color=color,
                             label=LBL_PCON,
                             linestyle='-',
                             linewidth=3, marker='None', markersize=4,
                             markerfacecolor=color, fillstyle='bottom',
                             alpha=1)
-    current_pdif = AX3.plot(PDIF, STRN, color=color,
+    current_pdif = AX3.plot(PDIF, FDEN, color=color,
                             label=LBL_PDIF,
                             linestyle='-',
                             linewidth=3, marker='None', markersize=4,
                             markerfacecolor=color, fillstyle='bottom',
                             alpha=1)
+    if plot_single == 0:
+        AX4.plot(TIME, FDEN, color=color,
+                 label=LBL_PCON,
+                 linestyle='-',
+                 linewidth=3, marker='None', markersize=4,
+                 markerfacecolor=color, fillstyle='bottom',
+                 alpha=1)
+        AX4A.plot(TIME, PRES_ARR.T,
+                  label=LBL_PRES,
+                  linestyle='--',
+                  linewidth=3, marker='None', markersize=4,
+                  markerfacecolor=color, fillstyle='bottom',
+                  alpha=1)
+        AX4A.legend(LBL_PRES, frameon=1, framealpha=.85, loc=0, fontsize=FS,
+                    numpoints=3)
+    elif plot_single == 1:
+        AX5.plot(TIME, FDEN, color=color,
+                 label=LBL_PCON,
+                 linestyle='-',
+                 linewidth=3, marker='None', markersize=4,
+                 markerfacecolor=color, fillstyle='bottom',
+                 alpha=1)
+        AX5A.plot(TIME, PRES_ARR.T,
+                  label=LBL_PRES,
+                  linestyle='--',
+                  linewidth=3, marker='None', markersize=4,
+                  markerfacecolor=color, fillstyle='bottom',
+                  alpha=1)
+        AX5A.legend(LBL_PRES, frameon=1, framealpha=.85, loc=0, fontsize=FS,
+                    numpoints=3)
+    elif plot_single == 2:
+        AX6.plot(TIME, FDEN, color=color,
+                 label=LBL_PCON,
+                 linestyle='-',
+                 linewidth=3, marker='None', markersize=4,
+                 markerfacecolor=color, fillstyle='bottom',
+                 alpha=1)
+        AX6A.plot(TIME, PRES_ARR.T,
+                  label=LBL_PRES,
+                  linestyle='--',
+                  linewidth=3, marker='None', markersize=4,
+                  markerfacecolor=color, fillstyle='bottom',
+                  alpha=1)
+        AX6A.legend(LBL_PRES, frameon=1, framealpha=.85, loc=0, fontsize=FS,
+                    numpoints=3)
+    elif plot_single == 3:
+        AX7.plot(TIME, FDEN, color=color,
+                 label=LBL_PCON,
+                 linestyle='-',
+                 linewidth=3, marker='None', markersize=4,
+                 markerfacecolor=color, fillstyle='bottom',
+                 alpha=1)
+        AX7A.plot(TIME, PRES_ARR.T,
+                  label=LBL_PRES,
+                  linestyle='--',
+                  linewidth=3, marker='None', markersize=4,
+                  markerfacecolor=color, fillstyle='bottom',
+                  alpha=1)
+        AX7A.legend(LBL_PRES, frameon=1, framealpha=.85, loc=0, fontsize=FS,
+                    numpoints=3)
+    plot_single = plot_single + 1
 
-    FDEN_LINES.append(current_fden)
+    FDEN_TIME_LINES.append(current_fden_time)
+    # PRES_TIME_LINES.append(current_pres_time)
+    # PPOR_TIME_LINES.append(current_ppor_time)
+    # PCON_TIME_LINES.append(current_pcon_time)
     PCON_LINES.append(current_pcon)
     PDIF_LINES.append(current_pcon)
+
+XMIN, XMAX = AX2.get_xlim()  # get bounds on volumetric strain axis
+XMIN = XMIN
+XMAX = XMAX
+AX3.set_xlim(xmin=XMIN, xmax=XMAX)
 
 #  CUSTOM TICK MARKS
 # TICK = np.arange(1, 1 + TEST_CNT, 1)
@@ -160,20 +255,29 @@ AX1.set_title("Loading Analysis: " + SAVE_DIR, fontsize=18)
 AX1.set_ylabel("Fractional Density", fontsize=FS)
 AX1.set_xlabel("Loading Time (sec)", fontsize=FS)
 AX2.set_ylabel(r'Volumetric Strain', fontsize=FS)
-AX2.set_xlabel("Confining Pressure (MPa)", fontsize=FS)
-AX3.set_ylabel('Volumetric Strain', fontsize=FS)
+# AX2.set_xlabel("Differential Pressure (MPa)", fontsize=FS)
+AX3.set_ylabel('Fractional Density', fontsize=FS)
 AX3.set_xlabel('Differential Pressure (MPa)', fontsize=FS)
+AX4A.set_ylabel('Pressure (MPa)', fontsize=FS)
+AX5A.set_ylabel('Pressure (MPa)', fontsize=FS)
+AX6A.set_ylabel('Pressure (MPa)', fontsize=FS)
+AX7A.set_ylabel('Pressure (MPa)', fontsize=FS)
+AX7.set_xlabel('Loading Time (sec)', fontsize=FS)
 AX1.grid()
 AX2.grid()
 AX3.grid()
+AX4.grid()
+AX5.grid()
+AX6.grid()
+AX7.grid()
 AX1.tick_params(labelsize=FS, pad=10)
 AX2.tick_params(labelsize=FS, pad=10)
 AX3.tick_params(labelsize=FS, pad=10)
 
 HANDLES_1, LABELS_1 = AX1.get_legend_handles_labels()
 #  bbox_to_anchor(xmin, ymin, xmax, ymax) ro bounding box of legend
-LGD1 = AX1.legend(HANDLES_1, LABELS_1, bbox_to_anchor=(1, 0, 1.2, 1),
-                  loc='upper left', borderaxespad=1, ncol=1,
+LGD1 = AX1.legend(HANDLES_1, LABELS_1, bbox_to_anchor=(0.0, -1.0, 1.0, 0.5),
+                  loc='upper left', borderaxespad=1, ncol=3,
                   fontsize=12)
 # HANDLES_3, LABELS_3 = AX3.get_legend_handles_labels()
 # LGD3 = AX3.legend(HANDLES_3, LABELS_3,
@@ -181,13 +285,13 @@ LGD1 = AX1.legend(HANDLES_1, LABELS_1, bbox_to_anchor=(1, 0, 1.2, 1),
 #                   loc='upper left', borderaxespad=1, ncol=1,
 #                   fontsize=12, numpoints=1, labelspacing=1, markerscale=0.75)
 
-# FIG1.tight_layout()
+FIG1.tight_layout()
 
 
-# if SAVE_PLOT == 1:
-#     FIG1_NAME = 'CreepTestSummary_175.pdf'
-#     FIG1_PATH = os.path.join(OUT_DIR, FIG1_NAME)
-#     FIG1.savefig(FIG1_PATH, bbox_extra_artists=(LGD1,), bbox_inches='tight')
+if SAVE_PLOT == 1:
+    FIG1_NAME = 'LoadingTestSummary_175.pdf'
+    FIG1_PATH = os.path.join(OUT_DIR, FIG1_NAME)
+    FIG1.savefig(FIG1_PATH, bbox_extra_artists=(LGD1,), bbox_inches='tight')
 
 if SHOW_PLOT == 1:
     plt.show()
